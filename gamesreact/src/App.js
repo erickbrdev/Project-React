76752de9b7a92850dea470376daf2e87296e8d2a
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-06 10:34:03 -0300

    add header

---
+++ gamesreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import ListMovies from './components/MovieList';
+// import CardMovie from './components/MovieCard';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
