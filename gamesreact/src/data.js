2f39440a34bd61e15ba298f0eee5e0e6c281122b
Erick-Andrade-Dev <erick.dev07@gmail.com>
2022-09-06 10:35:48 -0300

    create  arquivo data

---
+++ gamesreact/src/data.js
@@ -0,0 +1,39 @@
+const movies = [
+    {
+      title: 'Need for Speed Underground 2',
+      subtitle: 'Need for Speed Underground',
+      storyline: 'Stylize your car and accelerate',
+      rating: 9.5,
+      imagePath: 'https://http2.mlstatic.com/D_NQ_NP_904622-MLB48063613614_102021-W.jpg',
+    },
+    {
+      title: 'God of War 2',
+      subtitle: 'God of War',
+      storyline: 'Who will have the audacity to fight the God of War.',
+      rating: 9.0,
+      imagePath: "https://tm.ibxk.com.br/2021/03/15/15120758813131.jpg?ims=1200x675"
+    },
+    {
+      title: 'Naruto Shippūden: Ultimate Ninja Storm 4',
+      subtitle: 'Naruto Shippūden: Ultimate Ninja Storm 4',
+      storyline: 'Ninjutsu, taijutsu e genjutsu.',
+      rating: 10,
+      imagePath: 'https://cdn.cloudflare.steamstatic.com/steam/apps/349040/capsule_616x353.jpg?t=1611701005',
+    },
+    {
+      title: 'Call Of Duty',
+      subtitle: 'COD',
+      storyline: 'Fight for your homeland! ITS WAR!',
+      rating: 9.2,
+      imagePath: 'https://imgs.callofduty.com/content/dam/atvi/callofduty/cod-touchui/vanguard/meta-images/season-5/WZ_Metashare.jpg',
+    },
+    {
+      title: 'Street Fighter',
+      subtitle: 'Street Fighter',
+      storyline: 'Street fighting, and a lot of hadukennn.',
+      rating: 9,
+      imagePath: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/03/Sfv-logo.png/275px-Sfv-logo.png',
+    },
+  ];
+  
+  export default movies;
